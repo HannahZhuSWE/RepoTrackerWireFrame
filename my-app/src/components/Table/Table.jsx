69ab77e305fec81407c8c559187dce2d5e45cb7d
Hannah <t-hannahzhu@microsoft.com>
2021-01-22 18:14:13 -0500

    fixed table spacing

--- my-app/src/components/Table/Table.jsx
+++ my-app/src/components/Table/Table.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid, ColDef } from '@material-ui/data-grid';
 const table = (props) =>{
 
-    const columns = [
-        {field: 'repo', headerName:'Repo' },
-        {field: 'owner', headerName:'Owner'},
-        {field: 'githubLink', headerName:'Github Link'},
-        {field: 'articles', headerName:'articles', type: 'number'}
+    const columns: ColDef[] = [
+        {field: 'repo', headerName:'Repo', width:150 },
+        {field: 'owner', headerName:'Owner', width:200 },
+        {field: 'githubLink', headerName:'Github Link', width:350 },
+        {field: 'articles', headerName:'articles', textAlignment:"center", width:150 }
     ]
 
     const rows = require("../../assets/data/data.json");
 
     return (
-        <div style={{ width: "auto", height: 400 , marginTop: "auto"}}>
-            <DataGrid rows={rows} columns={columns} />
+        <div style={{ width: "100%", height: 400 , marginTop: "auto"}}>
+            <DataGrid 
+                rows={rows} 
+                columns={columns} 
+                disableExtendRowFullWidth={false}/>
         </div>
       );
 
